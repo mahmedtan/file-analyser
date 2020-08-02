85e3360cecee8c865af43ff082b1d0281d012cdd
Muhammad Ahmed <mahmedexec@gmail.com>
2020-08-02 17:57:52 +0500

    Project finalized

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+var upload = multer({ storage: storage });
 
 // require and use "multer"...
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.get("/hello", function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+app.post("/api/fileanalyse/", upload.single("upfile"), (req, res) => {
+  try {
+    var { filename: name, mimetype: type, size } = req.file;
+    res.json({ name, type, size });
+  } catch (err) {
+    console.log(err);
+    res.statusCode(400);
+  }
+});
+app.listen(3000, () => {
+  console.log("Node.js listening ...");
 });
